9de1bfaa35f588eba020414e23f2ea5cf64f764c
Julien Wangon <julien.wangon@gmail.com>
2024-02-14 10:44:34 +0100

    ADD: create Get User By Id method

--- epicbeats-back/repository/UserRepository.php
+++ epicbeats-back/repository/UserRepository.php
@@ -37,6 +37,53 @@ class UserRepository extends Database {
     }
 
 
+    /**
+      * Récupère les informations d'un utilisateur spécifique par son ID.
+      *
+      * Cette méthode exécute une requête SQL pour récupérer les informations d'un utilisateur, y compris son rôle,
+      * en utilisant son ID unique. Les informations retournées incluent l'ID de l'utilisateur, son nom d'utilisateur,
+      * son adresse email, et le nom de son rôle. Si aucun utilisateur n'est trouvé avec l'ID fourni, la méthode retourne null.
+      *
+      * @param int $userId L'ID de l'utilisateur à rechercher.
+      * @return array|null Un tableau associatif contenant les informations de l'utilisateur si trouvé, null sinon.
+      *
+      * @throws Exception Si une erreur de connexion à la base de données se produit,
+      * cette méthode propage une exception avec un message d'erreur adapté.
+    */
+    public function getUserByID($userId) {          
+        try {
+            $db = $this->getBdd();
+            $req = "SELECT u.id, u.userName, u.email, u.idRole, r.name
+                    FROM users u 
+                    JOIN roles r
+                    ON u.idRole = r.id
+                    WHERE u.id = :userId";
+
+            $stmt = $db->prepare($req);
+            $stmt->bindValue(":userId", $userId, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($userData) {      
+                $adjustedUserData = [
+                    "id" => $userData['id'],
+                    "userName" => $userData['userName'],
+                    "email" => $userData['email'],
+                    "roleName" => $userData['name'],
+                ];
+
+                return $adjustedUserData;
+            } else {
+                return null;
+            }
+        } catch (PDOException $e) {
+            $this->handleException($e, "recherche de l'utilisateur.");
+        }   
+    }
+
+
+
 
 
 }
