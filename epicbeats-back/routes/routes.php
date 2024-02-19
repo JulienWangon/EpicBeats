2ccb7cc701fd286d2a1c64104d36182372e3ad21
Julien Wangon <julien.wangon@gmail.com>
2024-02-19 17:09:41 +0100

    ADD: create GET instrumentals list route . TEST POSTMAN OK

--- epicbeats-back/routes/routes.php
+++ epicbeats-back/routes/routes.php
@@ -9,3 +9,62 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-CSRF-TOKEN");
 header("Access-Control-Allow-Credentials: true");
 
+
+require_once '../EpicBeats/epicbeats-back/controllers/InstrumentalController.php';
+require_once '../EpicBeats/epicbeats-back/repository/InstrumentalRepository.php';
+
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  http_response_code(200);
+  exit();
+}
+
+
+$instrumentalRepo = new InstrumentalRepository;
+
+$controllers = [
+  'instrumentals' => new InstrumentalController($instrumentalRepo),
+];
+
+
+$routes = [
+    'GET' => [
+        '/epicbeats/instrumentals' => [$controllers['instrumentals'], 'getInstrumentalsList'],
+    ],
+];
+
+
+
+    // EXTRACTION URI DE LA REQUETE
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    
+    $foundRoute = false;
+    
+    //Vérification des routes exactes
+    if(isset($routes[$requestMethod][$uri])) {
+      $routes[$requestMethod][$uri]();
+      $foundRoute = true;
+    }
+    
+    //Si la route n'est pas exacre contrôle les routes avec les regex
+    if (!$foundRoute) {
+    
+      foreach($routes[$requestMethod] as $pattern =>$function) {    
+          if (preg_match($pattern, $uri, $matches)) {
+     
+              array_shift($matches); 
+              $function(...$matches);
+              $foundRoute = true;
+              break;
+          }
+      }
+    }
+    
+    //Retourne une erreur 404 si la route n est pas trouvée
+    if(!$foundRoute) {
+    header('HTTP/1.1 404 Not Found');
+    echo json_encode(['status' => 'error', 'message' => 'Route not found']);
+    }
+    
