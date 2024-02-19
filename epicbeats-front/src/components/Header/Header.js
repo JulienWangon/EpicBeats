8d14871fd55edced1bc8bc19b3fa13451ee7c36e
Julien Wangon <julien.wangon@gmail.com>
2024-02-19 21:21:45 +0100

    ADD: create Header component

---
+++ epicbeats-front/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+         <button className="shadow__btn">catalog</button>
+    </header>
+  );
+};
+
+export default Header;
